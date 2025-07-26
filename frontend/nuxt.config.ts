022ce6816f96bdbd78b162e4bb29dabc67b90ff4
Kumagai <masafumi-kumagai-s@sensyn-robotics.com>
2025-07-27 01:07:44 +0900

    SPAモードに

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: false, // SPAモードに変更
   runtimeConfig: {
     public: {
       backendUrl: process.env.NUXT_PUBLIC_BACKEND_URL || 'http://localhost:3001'
